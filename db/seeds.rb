b6394e02a8eb80c0c6a430a62634eda35bd52436
Rafael Casique <recasique@gmail.com>
2020-08-21 10:18:23 +0200

    views created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+
+puts 'Creating data'
+
+10.times do
+  Flat.create(
+    name: Faker::Name.name,
+    address: Faker::Address.street_name,
+    description: Faker::Movies::StarWars.quote,
+    price_per_night: rand(100..250),
+    number_of_guests: rand(1..10)
+    )
+end
+
+puts 'Data completed!'
